b8198119895f5ac6edc46609edcb3170dd87c3ac
Luan Charlys <firminocharlys@gmail.com>
2023-10-06 13:27:27 -0300

    :feat: criacao de autenticacao do usuario logado

--- src/server/routes/routerUser.ts
+++ src/server/routes/routerUser.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { user } from '../conttrollers/users';
+import { midUserLogin } from '../conttrollers/shared/middlewares/authentication/user.authentication';
 
 const routesUser = express();
 
 routesUser.post('/user', user.createValidation, user.create);
 routesUser.post('/user/login', user.loginValidation, user.login);
+
+routesUser.use('/user', midUserLogin);
 
 export { routesUser };
