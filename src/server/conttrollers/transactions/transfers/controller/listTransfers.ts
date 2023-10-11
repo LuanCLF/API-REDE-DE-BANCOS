471e60956877bd829274e4c29ac0bf3c03fed6ba
Luan Charlys <firminocharlys@gmail.com>
2023-10-10 22:11:38 -0300

    :feat: rota para listar as tranferencias, com a opcao de listar as que o usuario enviou e as que recebeu

--- src/server/conttrollers/transactions/transfers/controller/listTransfers.ts
+++ src/server/conttrollers/transactions/transfers/controller/listTransfers.ts
@@ -1,3 +1,14 @@
 import { RequestHandler } from 'express';
+import { ListTransfers } from '../service/listTransfers.services';
 
-export const listTransfers: RequestHandler = async (req, res) => {};
+export const listTransfers: RequestHandler = async (req, res) => {
+  const { userID } = req.headers;
+  const page = Number(req.query.page) || 0;
+
+  let { originFrom } = req.query;
+  originFrom = originFrom !== 'out' ? '' : 'out';
+
+  const transfers = await ListTransfers(Number(userID), page, originFrom);
+
+  res.status(200).json(transfers);
+};
